146f4f880c0456c37650cc4b87fe672c87658956
Leonidas Yopan <leonidasyopan@gmail.com>
2025-07-25 11:05:04 -0300

    feat: integrate Firebase with environment configuration and analytics setup

---
+++ firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Conditionally initialize Analytics.
+// This is just to enable analytics tracking. The instance is not used in the app yet.
+isSupported().then(supported => {
+  if (supported) {
+    getAnalytics(app);
+  }
+});
+
+// Export the app instance in case it's needed for other Firebase services.
+export default app;
